dd57849082a0ee26cc4e0ef143a95d603b060be2
achraf <acherf2011@gmail.com>
2025-09-23 18:22:20 +0100

    last

--- UI/header_2/page.js
+++ UI/header_2/page.js
@@ -1,14 +1,14 @@
 
 import Link from "next/link";
 import styles from "./header.module.css"; 
-
+import Image from "next/image";
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
 
         <Link href="/admin" className={styles.logo} prefetch={true}>
-          MK exports
+          <Image src="../../public/mk exports.jpg" width={50} alt="mk exports" height={50} />
         </Link>
 
       </div>
