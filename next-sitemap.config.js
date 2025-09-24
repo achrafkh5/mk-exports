4a17a080675f694422545783921b7f73b98d5974
achraf <acherf2011@gmail.com>
2025-09-25 00:02:43 +0100

    last

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mk-exports.vercel.app',  // your domain
+  generateRobotsTxt: true, // also creates robots.txt
+}
