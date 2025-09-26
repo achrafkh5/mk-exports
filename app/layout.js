c265e2af164b27bc0e09ee42092f5f01f4872910
achraf <acherf2011@gmail.com>
2025-09-26 20:12:18 +0100

    pfp

--- app/layout.js
+++ app/layout.js
@@ -15,22 +15,39 @@ const geistMono = Geist_Mono({
 
 // app/layout.tsx
 export const metadata = {
-  title: "MK Exports -Algeria- Quality Products Worldwide",
+  title: "MK Exports - Algeria - Quality Products Worldwide",
   description: "Official website of MK Exports Algeria. Explore our high-quality products and services.",
-  keywords: ["MK Exports", "exports", "products", "international trade","Algerian products","global shipping"],
+  keywords: [
+    "MK Exports",
+    "exports",
+    "products",
+    "international trade",
+    "Algerian products",
+    "global shipping"
+  ],
   metadataBase: new URL("https://mk-exports.vercel.app"),
   openGraph: {
     title: "MK Exports - Quality Products Worldwide",
     description: "Official website of MK Exports.",
     url: "https://mk-exports.vercel.app",
     siteName: "MK Exports",
+    images: [
+      {
+        url: "/mk.png", // or full URL: "https://mk-exports.vercel.app/preview.jpg"
+        width: 1200,
+        height: 630,
+        alt: "MK Exports Algeria - Quality Products"
+      }
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "MK Exports",
     description: "Official website of MK Exports.",
+    images: ["/mk_exports_copy-removebg-preview.png"], // same image for Twitter
   },
 };
+
 
 
 export default function RootLayout({ children }) {
@@ -38,7 +55,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="./favicon.ico" />
+        <link rel="icon" href="/mk.png" />
         <title>MK Exports - Quality Products Worldwide</title>
         <meta name="description" content="Official website of MK Exports. Explore our high-quality products and services." />
         <meta name="keywords" content="MK Exports, exports, products, international trade" />
